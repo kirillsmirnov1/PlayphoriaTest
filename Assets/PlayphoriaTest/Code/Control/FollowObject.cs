6c5d4b696b4aa3adcf57c5528444d730c047f1fe
Kirill Smirnov <thegaldren@gmail.com>
2022-03-10 13:27:00 +0400

    Make camera follow player

---
+++ Assets/PlayphoriaTest/Code/Control/FollowObject.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace PlayphoriaTest.Control
+{
+    public class FollowObject : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private Vector3 gap = new Vector3(0, 10, 0);
+        [SerializeField] private float speed = 1f;
+        
+        private void OnValidate()
+        {
+            if (!Application.isPlaying)
+            {
+                transform.position = target.position + gap;
+            }
+        }
+
+        private void Update() 
+            => LerpToTarget();
+
+        private void LerpToTarget()
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position + gap, speed * Time.deltaTime);
+        }
+    }
+}
